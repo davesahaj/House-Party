c93fabf89f43c7d5be8fa36cd1f8613629ef995e
dave <sahajdave@gmail.com>
2021-03-30 14:49:41 +0530

    initial set up

---
+++ HouseParty/api/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+from .models import Room
+
+
+class RoomSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Room
+        fields = ('id', 'code', 'guests_can_pause', 'host', 'votes_to_skip',
+                  'created_at')
